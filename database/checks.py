22141022729d49bf8ac3923b2c9c6169563fe14e
DARKPOISON-yt <dashutosh06122004@gmail.com>
2022-04-03 11:55:04 +0530

    📹 Updating user_checks

--- database/checks.py
+++ database/checks.py
@@ -2,7 +2,7 @@ from database import connection as con
 import os.path
 import json
 
-async def user_check(uid):
+async def user_check_cash(uid):
     prefetcher_file = os.path.join(os.path.dirname(__file__), "./prefetch_data/cash_cache.json")
     with open(prefetcher_file) as f:
         cash_data = json.load(f)
@@ -14,6 +14,22 @@ async def user_check(uid):
         with open(prefetcher_file, "w") as f:
             json.dump(cash_data, f, indent=4)
 
+async def user_check_inventory(uid):
+    prefetcher_file = os.path.join(os.path.dirname(__file__), "./prefetch_data/inventory_cache.json")
+    with open(prefetcher_file) as f:
+        cash_data = json.load(f)
+    if str(uid) not in cash_data:
+        db = con.create_conn()
+        cursor = db.cursor()
+        cursor.execute(f"INSERT INTO INVENTORY(id) VALUES({uid})")
+        cash_data[str(uid)] = 20000
+        with open(prefetcher_file, "w") as f:
+            json.dump(cash_data, f, indent=4)
+
+async def user_check(uid):
+    await user_check_cash(uid)
+    await user_check_inventory(uid)
+    
 async def table_check():
     db = con.create_conn()
     cursor = db.cursor()
